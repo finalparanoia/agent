a522db6969c777a69dfb4b6bfb62541395eb867a
a <dev@company.com>
2026-06-03 14:39:18 +0800

    example

---
+++ src/agent/define/sfw.py
@@ -0,0 +1,17 @@
+WORLD_BOOK_DATA = {
+    "name": "拉海诺市",
+    "definitions": [
+        ""
+    ],
+    "characters": [
+        {
+            "name": "玩家",
+            "description": "一个看似平凡的高中生",
+        },
+    ],
+}
+
+
+ROUND = [
+    "摸摸路上的猫咪",
+]
